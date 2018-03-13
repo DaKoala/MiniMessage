d7e7ce6416df3d6cb411ceb70026a9721fa35b1f
billyzou <yz3352@nyu.edu>
2018-03-13 09:24:26 +0800

    test

--- run.py
+++ run.py
@@ -3,6 +3,6 @@ from __init__ import app
 
 if __name__ == "__main__":
     # On server
-    app.run(host="0.0.0.0", port=80)
+    # app.run(host="0.0.0.0", port=80)
     # For text
-    # app.run(host="127.0.0.1", port=5000)
+    app.run(host="127.0.0.1", port=5000)
