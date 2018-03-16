6c0fb511b7bd7c760d4c320483c7a6a522408e7e
billyzou <yz3352@nyu.edu>
2018-03-16 12:10:11 +0800

    add another arg for testing

--- run.py
+++ run.py
@@ -1,8 +1,14 @@
 from __init__ import app
-
-TEST = True
+import sys
 
 if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        raise ValueError("Please input exactly 1 argument!(Input 0 for test mode)")
+    if sys.argv[1] == '0':
+        TEST = True
+    else:
+        TEST = False
+
     # For test
     if TEST:
         app.debug = True
